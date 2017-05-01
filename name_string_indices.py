091eabcb9e182b8825ab194459e25c0f97bbc53e
Alexander Myltsev <alexander@myltsev.com>
2017-05-01 22:25:30 +0300

    Migrate to Spark 2.x

--- name_string_indices.py
+++ name_string_indices.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
+
 import json
 import os.path
 import shutil
-import re
 
 from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
 from settings import parse_spark, csvs_dir, mysql_export_dir
 
 output_dir_name_string_indices = os.path.join(csvs_dir, "name_string_indices")
@@ -14,48 +15,44 @@ def cleanup():
     shutil.rmtree(output_dir_name_string_indices, ignore_errors=True)
 
 
+def nullable(value):
+    return value if value else 'NULL'
+
+
 def name_string_index_result(value):
-    (((((((((((name_string_id, url), data_source_id), taxon_id), global_id), local_id),
-      nomenclatural_code_id), rank), accepted_taxon_id), classification_path),
-      classification_path_ids), classification_path_ranks) = value
-    url = url.replace("\t", " ")
-    out = "\t".join([str(data_source_id), str(name_string_id), str(url), str(taxon_id), str(global_id),
-                     str(local_id), str(nomenclatural_code_id), str(rank), str(accepted_taxon_id),
-                     str(classification_path), str(classification_path_ids), str(classification_path_ranks)])
-    return re.sub("\tNULL\t", "\t\\N\t", out)
+    name_json, rec = value
+    output = [
+        str(rec['data_source_id'])
+        , name_json['name_string_id']
+        , nullable(rec['url']).replace('\t', ' ')
+        , nullable(rec['taxon_id'])
+        , nullable(rec['global_id'])
+        , nullable(rec['local_id'])
+        , str(nullable(rec['nomenclatural_code_id']))
+        , nullable(rec['rank'])
+        , nullable(rec['classification_path'])
+        , nullable(rec['classification_path_ids'])
+        , nullable(rec['classification_path_ranks'])
+    ]
+    return '\t'.join(output)
 
 
 def main():
     cleanup()
 
     sc = SparkContext()
-    sql_context = SQLContext(sc)
+    spark = SparkSession(sc)
     path = os.path.join(mysql_export_dir, "name_string_indices.tsv")
-    df = sql_context.load(source='com.databricks.spark.csv', header='true', inferSchema='true', path=path,
-                          quote="\u0000", delimiter="\t")
 
-    names = df.rdd.map(lambda x: x["name"])
-    urls = df.rdd.map(lambda x: x["url"])
-    data_source_ids = df.rdd.map(lambda x: x["data_source_id"])
-    taxon_id = df.rdd.map(lambda x: x["taxon_id"])
-    global_id = df.rdd.map(lambda x: x["global_id"])
-    rank = df.rdd.map(lambda x: x["rank"])
-    accepted_taxon_id = df.rdd.map(lambda x: x["accepted_taxon_id"])
-    classification_path = df.rdd.map(lambda x: x["classification_path"])
-    classification_path_ids = df.rdd.map(lambda x: x["classification_path_ids"])
-    nomenclatural_code_id = df.rdd.map(lambda x: x["nomenclatural_code_id"])
-    local_id = df.rdd.map(lambda x: x["local_id"])
-    classification_path_ranks = df.rdd.map(lambda x: x["classification_path_ranks"])
+    df = spark.read.csv(path, header=True, inferSchema=True, sep='\t', nullValue='NULL')
 
-    parsed_names_json = parse_spark(sc, names) \
-        .map(lambda result: json.loads(result)["name_string_id"]) \
-        .zip(urls).zip(data_source_ids).zip(taxon_id) \
-        .zip(global_id).zip(local_id).zip(nomenclatural_code_id).zip(rank) \
-        .zip(accepted_taxon_id).zip(classification_path).zip(classification_path_ids) \
-        .zip(classification_path_ranks) \
-        .map(name_string_index_result)
+    names = df.select('name').rdd.map(lambda r: r['name'])
 
-    parsed_names_json.saveAsTextFile(output_dir_name_string_indices)
+    parse_spark(sc, names) \
+        .map(json.loads) \
+        .zip(df.rdd) \
+        .map(name_string_index_result) \
+        .saveAsTextFile(output_dir_name_string_indices)
 
 
 if __name__ == "__main__":
